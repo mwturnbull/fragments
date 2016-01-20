12343206e6c3c9b5f612857b8424f777c2a19e43
mwturnbull <androidmwt@gmail.com>
2016-01-20 12:02:55 +0000

    code removed

--- app/src/main/java/com/mwt/android/fragments/SecondActivity.java
+++ app/src/main/java/com/mwt/android/fragments/SecondActivity.java
@@ -1,9 +1,7 @@
 package com.mwt.android.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
 public class SecondActivity extends FragmentActivity {
 
@@ -11,17 +9,6 @@ public class SecondActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        FragmentManager manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.second_fragment_container);
-
-        if(fragment == null){
-            //fragment = new SecondFragment();
-            fragment = SecondFragment.newInstance(getIntent().getStringExtra("ABC"));
-            manager.beginTransaction()
-                    .add(R.id.second_fragment_container, fragment)
-                    .commit();
-        }
 
     }
 
--- app/src/main/java/com/mwt/android/fragments/SecondFragment.java
+++ app/src/main/java/com/mwt/android/fragments/SecondFragment.java
@@ -1,8 +1,6 @@
 package com.mwt.android.fragments;
 
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,16 +15,6 @@ import android.widget.TextView;
  */
 public class SecondFragment extends Fragment {
 
-    public static SecondFragment newInstance(String msg) {
-
-        Bundle args = new Bundle();
-        args.putString("QWERTY",  msg);
-
-        SecondFragment fragment = new SecondFragment();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public SecondFragment() {
         // Required empty public constructor
     }
@@ -38,18 +26,13 @@ public class SecondFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_second, container, false);
         TextView text = (TextView)view.findViewById(R.id.text_view_msg);
-        //String message = getActivity().getIntent().getStringExtra("ABC");
-        String message = getArguments().getString("QWERTY");
-        text.setText(message);
+
 
         Button back = (Button)view.findViewById(R.id.button_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.putExtra("QAZ", "Return Message");
-                getActivity().setResult(Activity.RESULT_OK, intent);
-                getActivity().finish();
+
             }
         });
         return view;
